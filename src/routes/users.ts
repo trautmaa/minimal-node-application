37ac519995ee86ba6dcb0a381534cc8b080f6e46
Alex T <alexjtrautman@gmail.com>
2019-12-14 18:28:54 -0500

    hookup all users functions to db

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -3,35 +3,54 @@ import { UserRepository } from "../user_repository";
 
 export const userRoutes = (app: Express, userRepo: UserRepository) => {
 
-    const users = [
-        {
-            id: "1",
-            username: "Robin Wieruch",
-        },
-        {
-            id: "2",
-            username: "Dave Davids",
-        },
-    ];
-
-    app.get("/users/:userID", (req, res) => {
-        return res.send(
-            users.filter((user) => user.id === req.params.userID),
+    // Get one
+    app.get("/users/:userId", (req, res) => {
+        const { userId } = req.params;
+        return userRepo.getById(userId).then(
+            (user) =>
+                res.send(user)
         );
     });
 
+    // Get all
     app.get("/users", (req, res) => {
-        const resp = userRepo.getAll().then((theUsers) => res.send(theUsers));
-        // return res.send(users);
+        return userRepo.getAll().then(
+            (theUsers) =>
+                res.send(theUsers)
+        );
     });
-    app.post("/users", (req, res) => {
-        return res.send("Received a POST HTTP method: users");
+
+    // Create
+    app.post("/users/:userId", (req, res) => {
+        const { userId } = req.params;
+        return userRepo.create(userId, 'email').then(
+            (user) =>
+                res.send(user)
+        );
     });
-    app.put("/users", (req, res) => {
-        return res.send("Received a PUT HTTP method: users");
+
+    // Update
+    app.put("/users/:userId", (req, res) => {
+        const { userId } = req.params;
+        return userRepo.update(
+            {
+                id: userId,
+                name: 'new name',
+                email: 'new email'
+            }
+        ).then(
+            (user) =>
+                res.send(user)
+        );
     });
-    app.delete("/users", (req, res) => {
-        return res.send("Received a DELETE HTTP method: users");
+
+    // Delete
+    app.delete("/users/:userId", (req, res) => {
+        const { userId } = req.params;
+        return userRepo.delete(userId).then(
+            (user) =>
+                res.send(user)
+        );
     });
 
 };
